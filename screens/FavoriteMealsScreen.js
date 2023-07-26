993a5714a182b38a33a076e16b350821de570cfa
Lambertn33 <lambertgithub@gmail.com>
2023-07-26 15:03:31 +0200

    feat: list fav items

--- screens/FavoriteMealsScreen.js
+++ screens/FavoriteMealsScreen.js
@@ -1,7 +1,33 @@
-import { Text } from "react-native"
+import { FlatList } from "react-native";
+import { useContext } from "react";
+import { FavoriteMealsContext } from "../context/FavoriteMealsContext";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const FavoriteMealsScreen = () => {
-    return <Text>Fav Meals</Text>
-}
+const FavoriteMealsScreen = ({ navigation }) => {
+  const favMealCtx = useContext(FavoriteMealsContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favMealCtx.ids.includes(meal.id)
+  );
+  const navigateHandler = (mealId) => {
+    navigation.navigate("MealDetailsScreen", {
+      mealId,
+    });
+  };
 
-export default FavoriteMealsScreen
+  const renderMeals = ({ item }) => {
+    return (
+      <MealItem meal={item} onPress={navigateHandler.bind(this, item.id)} />
+    );
+  };
+
+  return (
+    <FlatList
+      data={favoriteMeals}
+      keyExtractor={(item) => item.id}
+      renderItem={renderMeals}
+    />
+  );
+};
+
+export default FavoriteMealsScreen;
